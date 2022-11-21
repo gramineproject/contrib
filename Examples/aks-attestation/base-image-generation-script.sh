381b7d997837346d6cb50d078dfb2ad2898a92dd
aneessahib <anees.a.sahib@intel.com>
2022-11-21 23:34:04 +0530

    Update AKS attestation example to use Gramine 1.3.1
    Signed-off-by: aneessahib <anees.a.sahib@intel.com>

--- Examples/aks-attestation/base-image-generation-script.sh
+++ Examples/aks-attestation/base-image-generation-script.sh
@@ -1,17 +1,17 @@
-# Install Gramine, and build the ra-tls-secret-prov files and
-# relevant libraries to be used in the server and client Dockerfiles.
-/bin/sh install_gramine.sh
-
 # Create Server image
-cd gramine/CI-Examples/ra-tls-secret-prov
-make clean && make dcap
-cd ../../../
 docker build -f aks-secret-prov-server.dockerfile -t aks-secret-prov-server-img .
 
 # Create Client image
-cd gramine/CI-Examples/ra-tls-secret-prov
-make clean && make secret_prov_min_client
-cd ../../../
 docker build -f aks-secret-prov-client.dockerfile -t aks-secret-prov-client-img .
 
-rm -rf gramine/
+# Clone GSC repo
+git clone --depth 1 https://github.com/gramineproject/gsc.git
+cd gsc
+cp ../config.yaml.template config.yaml
+
+# Generate an unsigned GSC image
+./gsc build aks-secret-prov-client-img ../aks-secret-prov-client.manifest
+
+# Create a test signing key, and generate the signed image
+openssl genrsa -3 -aes128 -passout pass:test@123 -out enclave-key.pem 3072
+./gsc sign-image aks-secret-prov-client-img enclave-key.pem -p test@123
