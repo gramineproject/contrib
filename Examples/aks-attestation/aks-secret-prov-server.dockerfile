c443ea078fc865ddcb7216b5271b0d0d62e6047f
aisog <malgorzata.rembas@intel.com>
2023-05-18 11:03:03 +0200

    update ubuntu version to 20.04 and dependecies
    Signed-off-by: aisog <mrembas@seccon.pl>

--- Examples/aks-attestation/aks-secret-prov-server.dockerfile
+++ Examples/aks-attestation/aks-secret-prov-server.dockerfile
@@ -1,4 +1,4 @@
-FROM ubuntu:18.04
+FROM ubuntu:20.04
 
 RUN apt-get update \
     && env DEBIAN_FRONTEND=noninteractive apt-get install -y \
@@ -10,15 +10,15 @@ RUN apt-get update \
 # Here, the version of az-dcap-client should be in sync with the az-dcap-client
 # version used for quote generation. User can replace the below package with the
 # latest package.
-RUN wget https://packages.microsoft.com/ubuntu/18.04/prod/pool/main/a/az-dcap-client/az-dcap-client_1.10_amd64.deb \
- && dpkg -i az-dcap-client_1.10_amd64.deb
+RUN wget https://packages.microsoft.com/ubuntu/20.04/prod/pool/main/a/az-dcap-client/az-dcap-client_1.11_amd64.deb \
+ && dpkg -i az-dcap-client_1.11_amd64.deb
 
 WORKDIR /ra-tls-secret-prov
 
 COPY gramine/CI-Examples/ra-tls-secret-prov/ssl ./ssl
-COPY gramine/CI-Examples/ra-tls-secret-prov/files ./files
+COPY gramine/CI-Examples/ra-tls-secret-prov/helper-files ./files
 
-COPY gramine/CI-Examples/ra-tls-secret-prov/secret_prov_server_dcap /usr/local/bin
+COPY gramine/CI-Examples/ra-tls-secret-prov/secret_prov /usr/local/bin
 
 RUN echo 'deb [arch=amd64] https://download.01.org/intel-sgx/sgx_repo/ubuntu bionic main' \
     > /etc/apt/sources.list.d/intel-sgx.list \
@@ -29,5 +29,4 @@ RUN curl -fsSLo /usr/share/keyrings/gramine-keyring.gpg https://packages.gramine
 RUN echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/gramine-keyring.gpg] https://packages.gramineproject.io/ stable main' | tee /etc/apt/sources.list.d/gramine.list
 RUN apt-get update
 RUN apt-get install -y gramine-dcap
-
-ENTRYPOINT ["secret_prov_server_dcap"]
+ENTRYPOINT ["secret_prov"]
--- Examples/aks-attestation/install_gramine.sh
+++ Examples/aks-attestation/install_gramine.sh
@@ -2,14 +2,27 @@ sudo curl -fsSLo /usr/share/keyrings/gramine-keyring.gpg https://packages.gramin
 echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/gramine-keyring.gpg] https://packages.gramineproject.io/ stable main' | sudo tee /etc/apt/sources.list.d/gramine.list
 
 curl -fsSL https://download.01.org/intel-sgx/sgx_repo/ubuntu/intel-sgx-deb.key | sudo apt-key add -
-echo 'deb [arch=amd64] https://download.01.org/intel-sgx/sgx_repo/ubuntu bionic main' | sudo tee /etc/apt/sources.list.d/intel-sgx.list
+echo 'deb [arch=amd64] https://download.01.org/intel-sgx/sgx_repo/ubuntu $(lsb_release -cs) main' | sudo tee /etc/apt/sources.list.d/intel-sgx.list
+
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 
 sudo apt-get update
 sudo apt-get install gramine-dcap # for out-of-tree DCAP driver (required by AKS Ubuntu 18.04 distro)
 
+sudo apt-get install libsgx-dcap-quote-verify-dev
+
+######### dependences
+sudo apt-get install -y build-essential \
+    autoconf bison gawk nasm ninja-build pkg-config python3 python3-click \
+    python3-jinja2 python3-pip python3-pyelftools wget
+sudo python3 -m pip install 'meson>=0.56' 'tomli>=1.1.0' 'tomli-w>=0.4.0'
+
+##require pip3 install protobuf==3.20.3
+
 gramine-sgx-gen-private-key
 
-git clone --depth 1 https://github.com/gramineproject/gramine.git --branch v1.2
+git clone --depth 1 https://github.com/gramineproject/gramine.git --branch v1.4
 
 # Copy dummy server certificate with Common Name as "<AKS-DNS-NAME.*.cloudapp.azure.com>
 cp -r ssl/* gramine/CI-Examples/ra-tls-secret-prov/ssl
+
