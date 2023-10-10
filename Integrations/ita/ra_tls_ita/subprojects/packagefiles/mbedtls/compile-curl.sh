5c9e4fffbaa7bd0a01f31bbd593471704d799c7a
Gordon King <gordon.king@intel.com>
2023-10-10 10:32:41 -0700

    [Integrations/ita] Add RA-TLS/SecretProv libs for Intel Trust Authority (ITA)
    This commit adds the sources, documentation and tests for
    `libra_tls_verify_ita.so` and `libsecret_prov_verify_ita.so` libraries,
    used for integration of Gramine with ITA.
    
    Signed-off-by: Scott Raynor <scott.raynor@intel.com>
    Signed-off-by: Gordon King <gordon.king@intel.com>

---
+++ Integrations/ita/ra_tls_ita/subprojects/packagefiles/mbedtls/compile-curl.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -ex
+
+CURRENT_SOURCE_DIR="$1"
+VENDOR_SOURCE_DIR="$2"
+CURRENT_BUILD_DIR="$3"
+PRIVATE_DIR="$4"
+SUBPROJ_ROOT="$5"
+shift 5
+
+rm -rf "$PRIVATE_DIR"
+
+cp -ar "$VENDOR_SOURCE_DIR" "$PRIVATE_DIR"
+cp "$CURRENT_SOURCE_DIR"/include/mbedtls/*.h "$PRIVATE_DIR"/include/mbedtls/
+patch -p1 --directory "$PRIVATE_DIR" <"$CURRENT_SOURCE_DIR"/enforce-aes-ni.patch
+
+make -C "$PRIVATE_DIR" lib SUFFIX="''" install DESTDIR="$SUBPROJ_ROOT"/mbedtls-curl
+
+for output in $@
+do
+    cp -a "$PRIVATE_DIR"/library/"$(basename "$output")" "$output"
+done
